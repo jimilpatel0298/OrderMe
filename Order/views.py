8a3613af9e3d1629ece9083bb4a4b93afc381325
Jimil Patel <jimilpatel0298@gmail.com>
2021-04-18 18:10:39 +0530

    Update views.py

--- Order/views.py
+++ Order/views.py
@@ -107,7 +107,7 @@ def get_addons(requests, size_id):
 
 
 # place a new order
-@api_view(["POST"])
+@api_view(["POST"]), @csrf_exempt
 def place_order(request):
     if request.method == "POST":
         try:
