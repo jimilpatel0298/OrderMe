69a16a37b745ea7182a42027832d8cf4df443bba
Jimil Patel <jimilpatel0298@gmail.com>
2021-06-25 04:24:20 +0530

    Changes v1.0

--- OrderMe/settings.py
+++ OrderMe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=4^1o$=9-#!p4&n6!6q)#m$v)crood+h+hcll6tysoi@4gh7sy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -88,7 +88,10 @@ REST_FRAMEWORK = {
 'DEFAULT_AUTHENTICATION_CLASSES': [
     # 'rest_framework.authentication.SessionAuthentication',
     'rest_framework.authentication.BasicAuthentication',
-]
+],
+'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 WSGI_APPLICATION = 'OrderMe.wsgi.application'
